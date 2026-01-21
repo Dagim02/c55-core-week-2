4d3337eae354c08952b0d23f0ba105e7511cef8d
dagimgit config --global user.email dagimvanhaileselassie@gmail.com <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2026-01-21 19:39:22 +0100

    Fix formating and empty lines

--- task-3/converter.js
+++ task-3/converter.js
@@ -2,17 +2,19 @@ import promptSync from 'prompt-sync';
 const prompt = promptSync();
 // Exchange rate for EUR/USD (How much 1 EUR is in USD)
 const EUR_USD_RATE = 1.1643;
+
 // Menu display
 // BUG 1 FIXED: "conole.log" was misspelled, corrected to console.log
 console.log("Hello and welcome to the currency converter. Please choose:");
 console.log("1: Convert EUR to USD");
 console.log("2: Convert USD to EUR");
-// FEATURE added to Option 3 for displaying exchange rate
-console.log("3: Display the current exchange rate");
+console.log("3: Display the current exchange rate");  // FEATURE added to Option 3 for displaying exchange rate
 const menuSelection = prompt("Select your option [1, 2, or 3]:");
+
+console.log("\n");
+
 if (menuSelection === "1") {
   // EUR to USD
-  console.log("");
   const eurAmountInput = prompt("Enter amount in EUR:");
   const eurAmountNum = Number(eurAmountInput);
   // BUG 3 FIXED: Condition was wrong (eurAmountNum > 0)
@@ -24,7 +26,6 @@ if (menuSelection === "1") {
   }
 } else if (menuSelection === "2") {
   // USD to EUR
-  console.log("");
   const usdAmountInput = prompt("Enter amount in USD:");
   const usdAmountNum = Number(usdAmountInput);
   if (Number.isNaN(usdAmountNum) || usdAmountNum < 0) {
@@ -37,7 +38,6 @@ if (menuSelection === "1") {
   }
 } else if (menuSelection === "3") {
     // FEATURE added to Show exchange rate
-    console.log("");
     console.log(`The current exchange rate is 1 EUR = ${EUR_USD_RATE} USD.`);
 } else {
     console.log("Invalid selection. Please choose either 1, 2, or 3.");
